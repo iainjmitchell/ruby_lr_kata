cbcee55a85e8628b81d046a07f97fd5451ab2186
iainjmitchell <iainjmitchell@gmail.com>
2013-03-17 18:12:24 +0000

    Finished third  test finished refactoring

--- test_booking.rb
+++ test_booking.rb
@@ -30,6 +30,21 @@ class BookingTests < Test::Unit::TestCase
             .total
         assert_equal(price_of_one_night_in_single_room_at_palace_hotel, @displayed_total)
     end
+
+    def test_When_booking_two_nights_in_single_room_at_palace_hotel_Then_displayed_total_is_126
+        price_of_one_night_in_single_room_at_palace_hotel = Money.new(amount: 63)
+        hotel_room_stay = HotelRoomStay.new(
+            hotel: 'Palace Hotel',
+            room_type: 'Single Room',
+            number_of_nights: 2
+        )
+        room_type_price_repository = FakeRoomTypePriceRepository.new(
+            {hotel_room_stay.room_type => price_of_one_night_in_single_room_at_palace_hotel})
+        Booking.new(self, room_type_price_repository)
+            .add(hotel_room_stay)
+            .total
+        assert_equal(Money.new(amount: 126), @displayed_total)
+    end
     
     def show_total(total)
         @displayed_total = total
@@ -53,8 +68,11 @@ class Booking
     end
     
     def add(hotel_room_stay)
-        hotel_booking = HotelBooking.new(room_type: hotel_room_stay.room_type)
-        @hotel.book_room(hotel_booking)
+        hotel_booking = HotelBooking.new(
+            room_type: hotel_room_stay.room_type, 
+            number_of_nights: hotel_room_stay.number_of_nights
+        )
+        @hotel.book_room(hotel_booking) 
         self
     end
 
@@ -64,10 +82,11 @@ class Booking
 end
 
 class HotelBooking
-    attr_reader :room_type
+    attr_reader :room_type, :number_of_nights
 
     def initialize(values)
         @room_type = values[:room_type]
+        @number_of_nights = values[:number_of_nights]
     end
 end
 
@@ -79,23 +98,24 @@ class Hotel
 
     def book_room(hotel_booking)
         room_price = @room_type_price_repository.get(hotel_booking.room_type)
-        @booking_total.increment_by(room_price)
+        hotel_booking.number_of_nights.times do
+            @booking_total.increment_by(room_price)
+        end
     end
 end
-
-
 
 class BookingTotal
     def initialize(booking_display)
         @booking_display = booking_display
+        @total_amount = 0
     end
 
-    def increment_by(amount)
-        @total_amount = amount
+    def increment_by(money)
+        @total_amount += money.amount
     end
 
     def show
-        @booking_display.show_total(@total_amount)
+        @booking_display.show_total(Money.new(amount: @total_amount))
     end
 end
 
